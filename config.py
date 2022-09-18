f2219f99a6f4e527d7bb5d5d43586271eb8d6182
Lagon <109609208+codeslagon@users.noreply.github.com>
2022-09-18 14:59:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-SPOTIPY_CLIENT_ID = # spotify developer portal client_id
-SPOTIPY_CLIENT_SECRET = # spotify developer portal client_secret_id
+SPOTIPY_CLIENT_ID = f10f56d793d34dc7918abc5e6b59c02d # spotify developer portal client_id
+SPOTIPY_CLIENT_SECRET = 6efe7650b33d41768c4c1df1dc450993 # spotify developer portal client_secret_id
 SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
 SCOPE = "user-read-currently-playing"
 MARKET = "TR"
-api_id = # my.telegram.org app_id
-api_hash = # my.telegram.org api_hash
-token = # t.me/BotFather new bot token
-chat = # telegram chat id t.me/ReWoxiSpotify
+api_id = 7257970 # my.telegram.org app_id
+api_hash = a75ce5263a6cac50d4acdd4f5d5fb5d3 # my.telegram.org api_hash
+token = 5661322133:AAGIREru_3YdMmyv-irgV_PG-a9pG9HHwiw # t.me/BotFather new bot token
+chat = -1001624948735 # telegram chat id t.me/ReWoxiSpotify
 msg_id = # telegram message_id
